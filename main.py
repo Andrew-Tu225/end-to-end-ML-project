c2323035404ee764ec6b60b9cb4d93fbd27b4176
Andrew Tu <andrewtu335@gmail.com>
2026-05-23 12:31:31 -0400

    create data validation pipeline

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from src.mlProject.pipeline.data_validation import DataValidationTrainingPipeline
 from src.mlProject import logger
 from src.mlProject.pipeline.data_ingestion import DataIngestionTrainingPipeline
 
@@ -5,6 +6,16 @@ STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
---
+++ src/mlProject/components/data_validation.py
@@ -0,0 +1,31 @@
+import os
+from mlProject import logger
+import pandas as pd
+
+from src.mlProject.entity.config_entity import DataValidationConfig
+
+class DataValiadtion:
+    def __init__(self, config: DataValidationConfig):
+        self.config = config
+
+    def validate_all_columns(self) -> bool:
+        try:
+            validation_status = None
+            data = pd.read_csv(self.config.unzip_data_dir)
+            all_cols = list(data.columns)
+
+            all_schemas = self.config.all_schema.keys()
+
+            for col in all_cols:
+                if not col in all_schemas:
+                    validation_status = False
+                    with open(self.config.STATUS_FILE, 'w') as f:
+                        f.write(f"Validation status: {validation_status}")
+            else:
+                validation_status = True
+                with open(self.config.STATUS_FILE, 'w') as f:
+                    f.write(f"Validation status: {validation_status}")
+
+            return validation_status
+        except Exception as e:
+            raise e
